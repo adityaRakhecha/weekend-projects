d5f44b3443082e709d593199f1a999601f3a5e22
Aditya Rakhecha <dashingaditya.rakhecha@gmail.com>
2018-10-29 18:15:16 +0530

    Create ex_SelectCheckboxes.java

---
+++ java_applet_programs/ex_SelectCheckboxes.java
@@ -0,0 +1,47 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+<applet code="ex_SelectCheckboxes" height=500 width=500></applet>
+*/
+
+public class ex_SelectCheckboxes extends Applet implements ItemListener
+{
+	String msg = "";
+	Checkbox sAll, op1, op2, op3, op4;
+
+	public void init()
+	{
+		sAll = new Checkbox("Select all", false);
+		op1 = new Checkbox("Option 1", true);
+		op2 = new Checkbox("Option 2", false);
+		op3 = new Checkbox("Option 3", false);
+		op4 = new Checkbox("Option 4", false);
+
+		add(sAll);
+		add(op1);
+		add(op2);
+		add(op3);
+		add(op4);
+
+		sAll.addItemListener(this);
+		op1.addItemListener(this);
+		op2.addItemListener(this);
+		op3.addItemListener(this);
+		op4.addItemListener(this);
+	}
+
+	public void itemStateChanged(ItemEvent ie)
+	{
+		if(sAll.getState() == true)
+		{
+			op1.setState(true);
+			op2.setState(true);
+			op3.setState(true);
+			op4.setState(true);
+		}
+		repaint();
+	}
+
+}
